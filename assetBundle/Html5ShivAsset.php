b3f7bd993b994630961462146edcfed3a8d50770
E-Kevin <e-kevin@qq.com>
2017-04-29 08:35:46 +0800

    添加Html5shiv.js和respond.js

---
+++ assetBundle/Html5ShivAsset.php
@@ -0,0 +1,15 @@
+<?php
+namespace wonail\base\assetBundle;
+
+use yii\web\AssetBundle;
+
+class Html5ShivAsset extends AssetBundle
+{
+
+    public $sourcePath = null;
+    public $jsOptions = ['condition' => 'lte IE9'];
+    public $js = [
+        'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js'
+    ];
+
+}
---
+++ assetBundle/RespondAsset.php
@@ -0,0 +1,15 @@
+<?php
+namespace wonail\base\assetBundle;
+
+use yii\web\AssetBundle;
+
+class RespondAsset extends AssetBundle
+{
+
+    public $sourcePath = null;
+    public $jsOptions = ['condition' => 'lte IE9'];
+    public $js = [
+        'https://oss.maxcdn.com/respond/1.4.2/respond.min.js'
+    ];
+
+}
